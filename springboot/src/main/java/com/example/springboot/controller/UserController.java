498247de413454cd7eee91ee3cceac312f373fdd
zervan <nickmechlin01@outlook.com>
2022-11-16 22:01:39 +0800

    add

--- springboot/src/main/java/com/example/springboot/controller/UserController.java
+++ springboot/src/main/java/com/example/springboot/controller/UserController.java
@@ -4,11 +4,13 @@ import com.example.springboot.entity.User;
 import com.example.springboot.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
 
