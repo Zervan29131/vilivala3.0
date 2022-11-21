eb5a1f3a645a3bae5e790622f8445744e837a821
zervan <nickmechlin01@outlook.com>
2022-11-21 23:20:46 +0800

    add

--- springboot/src/main/java/com/example/springboot/mapper/UserMapper.java
+++ springboot/src/main/java/com/example/springboot/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package com.example.springboot.mapper;
 
 import com.example.springboot.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -12,4 +11,12 @@ public interface UserMapper {
     @Select("SELECT * from sys_user")
     List<User> findAll();
 
+    @Insert("INSERT into sys_user(username, password,nickname,email,phone,address) VALUES (#{username}, #{password}," +
+            " #{nickname}, #{email},#{phone}, #{address})")
+    int insert(User user);
+
+    int update(User user);
+
+    @Delete("delete from sys_user where id = #{id}")
+    Integer deleteById(@Param("id") Integer id);
 }
